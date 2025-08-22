502382d712cafe535285a895028e70523259f191
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2025-08-23 02:19:13 +0300

    Remove unused import in BalanceInfoScreen for cleaner code

--- app/src/main/java/com/apka/spendly/ui/screens/BalanceInfo/BalanceInfoScreen.kt
+++ app/src/main/java/com/apka/spendly/ui/screens/BalanceInfo/BalanceInfoScreen.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
