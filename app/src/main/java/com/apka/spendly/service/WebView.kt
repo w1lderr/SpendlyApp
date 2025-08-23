64672d56909760b814770b8eb7f9e101bb55530c
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2025-08-24 00:20:09 +0300

    Created web view screen, and added privacy policy, terms of use screen.

---
+++ app/src/main/java/com/apka/spendly/service/WebView.kt
@@ -0,0 +1,26 @@
+package com.apka.spendly.service
+
+import android.annotation.SuppressLint
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun WebViewScreen(url: String) {
+    AndroidView(factory = { context ->
+        WebView(context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            webViewClient = WebViewClient()
+        }
+    }, update = { webView ->
+        webView.loadUrl(url)
+    })
+}
--- app/src/main/java/com/apka/spendly/ui/screens/Settings/PrivacyPolicyScreen.kt
+++ app/src/main/java/com/apka/spendly/ui/screens/Settings/PrivacyPolicyScreen.kt
@@ -1,28 +1,76 @@
 package com.apka.spendly.ui.screens.Settings
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.apka.spendly.service.WebViewScreen
 
 @Composable
 fun PrivacyPolicyScreen(
     navController: NavController,
     paddingValues: PaddingValues
 ) {
-
-
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF1A1A1A))
             .padding(paddingValues),
     ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp, top = 8.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(RoundedCornerShape(90.dp))
+                    .background(Color(0xFF1F1F1F))
+                    .border(
+                        border = BorderStroke(1.dp, Color(0xFF313131)),
+                        shape = RoundedCornerShape(90.dp)
+                    )
+                    .clickable(
+                        onClick = {
+                            navController.popBackStack()
+                        }), contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    modifier = Modifier.size(28.dp),
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+        }
 
+        Spacer(modifier = Modifier.height(20.dp))
+
+        WebViewScreen("https://spendly-privacy-policy-and-terms-of.vercel.app/privacy-policy")
     }
 }
