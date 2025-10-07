5aa926d51fc0c2ef73c8cc90226ace05f04ee59c
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2025-10-07 23:03:27 +0300

    Remove unused imports in StatChallengeItem

--- app/src/main/java/com/apka/spendly/ui/screens/FinanceTips/FinanceTipsScreen.kt
+++ app/src/main/java/com/apka/spendly/ui/screens/FinanceTips/FinanceTipsScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -36,7 +35,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.apka.spendly.data.dto.FinanceTipDTO
