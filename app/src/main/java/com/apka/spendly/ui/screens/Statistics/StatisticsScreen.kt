e6c9eef441fbc502d5181b7cf60763e17ee2cbac
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2025-10-29 17:50:39 +0200

    Remove unused imports in StatisticsScreen

--- app/src/main/java/com/apka/spendly/ui/screens/Statistics/StatisticsScreen.kt
+++ app/src/main/java/com/apka/spendly/ui/screens/Statistics/StatisticsScreen.kt
@@ -5,10 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
