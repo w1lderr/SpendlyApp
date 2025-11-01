b18dd449e2c7dc1977b3db46cfcd1605d8e87b01
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2025-11-01 13:19:55 +0200

    Update Kotlin serialization and KSP plugin versions in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp")
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("plugin.serialization") version "2.2.21"
 }
 
 android {
