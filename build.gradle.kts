00d613698a785034c4143fd358637d44bee12d82
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2025-11-07 14:49:10 +0200

    Update KSP and Google Services plugin versions; bump Firebase BOM and Kotlin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.2.21-2.0.4" apply false
-    id("com.google.gms.google-services") version "4.4.3" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
