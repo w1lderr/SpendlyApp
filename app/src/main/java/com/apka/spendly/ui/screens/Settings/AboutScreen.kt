83e11172d0859152199c70a47dca421a5016dd0b
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2025-11-28 19:05:18 +0200

    Update version label in AboutScreen to v1.7.1

--- app/src/main/java/com/apka/spendly/ui/screens/Settings/AboutScreen.kt
+++ app/src/main/java/com/apka/spendly/ui/screens/Settings/AboutScreen.kt
@@ -120,7 +120,7 @@ fun AboutScreen(
             horizontalArrangement = Arrangement.Start
         ) {
             Text(
-                text = "Версія: v1.7",
+                text = "Версія: v1.7.1",
                 fontSize = 21.sp,
                 fontWeight = FontWeight.Medium,
                 color = Color.White,
